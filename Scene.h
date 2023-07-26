ec98af8277c277cf64723b9b56fe8043071f82d5
bunyaminAkcay <bunyamin.akcay@outlook.com>
2023-07-27 00:47:23 +0300

    Scene.h modified.

--- Scene.h
+++ Scene.h
@@ -4,15 +4,55 @@
 #include <iostream>
 #include <list>
 #include <stdlib.h>
+#include <Windows.h>
+
+// Mingw headers are wrong. GetCurrentConsoleFont and _CONSOLE_FONT_INFO is not declared.
+// if you are using any c++ compiler instead of mingw. This part is not needed probably.
+typedef struct _CONSOLE_FONT_INFO {
+    DWORD  nFont;
+    COORD  dwFontSize;
+} CONSOLE_FONT_INFO, *PCONSOLE_FONT_INFO;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+BOOL WINAPI GetCurrentConsoleFont(HANDLE hConsoleOutput,BOOL bMaximumWindow,PCONSOLE_FONT_INFO lpConsoleCurrentFont);
+#ifdef __cplusplus
+}
+#endif
+// end of fix.
+
+struct Console{
+    short consoleSizeX;
+    short consoleSizeY;
+    short fontSizeX;
+    short fontSizeY;
+};
 
 class Scene{
 private:
 
     float angle = 90;
 
+
+    Console getConsoleInfo(){
+        HANDLE hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+        struct Console console;
+        if (hConsoleOutput != INVALID_HANDLE_VALUE){
+            PCONSOLE_SCREEN_BUFFER_INFO screenBufferInfo;
+            CONSOLE_FONT_INFO fontInfo;
+
+            bool a = GetCurrentConsoleFont(hConsoleOutput, false, &fontInfo);
+            bool b = GetConsoleScreenBufferInfo(hConsoleOutput, screenBufferInfo);
+            console = { (short)(screenBufferInfo->srWindow.Right - screenBufferInfo->srWindow.Left), (short)(screenBufferInfo->srWindow.Bottom - screenBufferInfo->srWindow.Top), fontInfo.dwFontSize.X, fontInfo.dwFontSize.Y};
+        
+        }
+        return console;
+    }
+
     void clearScreen(){
         system("cls");
-    }
+    }   
     
 public:
     Camera* camera;
@@ -29,16 +69,15 @@ public:
     void RenderScene(){
         
         std::string shot = "";
+        struct Console console = getConsoleInfo();
         
-        for (int i = 0; i < camera->resolution.y; i++){
-            for (int j = 0; j < camera->resolution.x; j++){
-
-
+        for (int i = 0; i < console.consoleSizeY; i++){
+            for (int j = 0; j < console.consoleSizeX; j++){
                 shot.append("@");
             }
             shot.append("\n");
         }
-
+        
         clearScreen();
         std::cout << shot;
     }
